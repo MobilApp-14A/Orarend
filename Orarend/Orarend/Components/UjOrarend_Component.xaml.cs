b52b7aed5feb36872e8f9410c9a71214c2a1bdee
VargaBalazsAdam <v.balazs.adam@gmail.com>
2023-01-06 17:14:54 +0100

    MainPage xaml

---
+++ Orarend/Orarend/Components/UjOrarend_Component.xaml.cs
@@ -0,0 +1,9 @@
+namespace Orarend.Components;
+
+public partial class UjOrarend_Component
+{
+	public UjOrarend_Component()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Orarend/Orarend/Models/Orarend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orarend.Models
+{
+    internal class Orarend
+    {
+        public string nev { get; set; } = string.Empty;
+        public int orakHossza { get; set; } //percben
+        public int szunetekHossza { get; set; }
+        public int nagySzunetHossza { get; set; }
+        public int hanyadikOraUtanVanNagySzunet { get; set; }
+        public List<Ora> orak { get; set; } //név, tanár, terem
+    }
+}
