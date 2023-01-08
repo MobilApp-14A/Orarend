3714d2dbf1eab2d5b66e2cb58006bca2f8c67b3c
WittmanBence <112657220+WittmanBence@users.noreply.github.com>
2023-01-08 13:09:08 +0100

    Wittman Bence Tamás: Adatbázis készítése

---
+++ Orarend/Orarend/Data/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.Collections.ObjectModel;
+using Orarend.Models;
+
+namespace Orarend.Data
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection connection;
+    
+        public Database(string dbPath)
+        {
+            connection = new SQLiteAsyncConnection(dbPath);
+            connection.CreateTableAsync<Orarendek>().Wait();
+        }
+
+        internal void OrarendMentes(Orarendek orarend)
+        {
+            connection.InsertAsync(orarend);
+        }
+
+        internal bool OrarendTorles(Orarendek kivalasztott)
+        {
+            return connection.DeleteAsync(kivalasztott).Result == 1;
+        }
+    }
+}
---
+++ Orarend/Orarend/Models/Orarendek.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orarend.Models
+{
+    internal class Orarendek
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public List<Ora> orak { get; set; } //név, tanár, terem
+    }
+}
